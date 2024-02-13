1768b55cbda2dd7b279635d1d9bb1b53e049a097
Yann D'Souza <yannssouza@outlook.com>
2024-02-13 15:22:15 -0300

    javascript adicionado v2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const form = document.getElementById("form");
-
-  form.addEventListener("submit", function (event) {
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.getElementById('form');
+  const overlayMensagem = document.getElementById('overlayMensagem');
+  const textoMensagem = document.getElementById('textoMensagem');
+  
+  form.addEventListener('submit', function(event) {
     event.preventDefault();
-
-    const nome = document.getElementById("nome").value;
-    const sobrenome = document.getElementById("lname").value;
-    const senha = document.getElementById("senha").value;
-    const mensagem = document.getElementById("mensagem").value;
-
-    if (nome === "" || sobrenome === "" || senha === "" || mensagem === "") {
-      alert("Por favor, preencha todos os campos.");
-      return;
+    
+    const nome = document.getElementById('nome').value;
+    const sobrenome = document.getElementById('nomel').value;
+    const senha = document.getElementById('senha').value;
+    const mensagem = document.getElementById('mensagem').value;
+    
+    if (nome === '' || sobrenome === '' || senha === '' || mensagem === '') {
+      alert('Por favor, preencha todos os campos.');
+      return; 
     }
-
-    console.log("formulário enviado com sucesso");
-    form.submit();
+    
+    textoMensagem.innerText = 'Formulário enviado com sucesso!';
+    overlayMensagem.style.display = 'block';
   });
 });
